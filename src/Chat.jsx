a0bf683ef06b4cb3583cde575c05b36e7aa7028b
Mohamad Salehfar <mohamad.salehfar1998@gmail.com>
2024-12-12 20:58:18 +0330

    add room

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,21 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { socket } from "./socket/socket";
 import { useDispatch, useSelector } from "react-redux";
 import { addUserName, deleteMessage, updated } from "./feature/chatSlice";
 
 function Chat() {
+  const [room, setRoom] = useState("");
   const [messageInput, setMessageInput] = useState("");
   const { message, userName } = useSelector((state) => state.chat.user);
   console.log(userName, message);
 
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    socket.on("send-username", (userName) => {
-      dispatch(addUserName(userName));
-    });
-  }, [dispatch]);
 
   useEffect(() => {
     socket.on("recive-message", (message) => {
@@ -26,11 +21,23 @@ function Chat() {
       dispatch(messageId);
     });
 
+    socket.on("send-username", (userName) => {
+      dispatch(addUserName(userName));
+    });
+
     return () => {
       socket.off("recive-message");
       socket.off("delete-message-form-others");
+      socket.off("send-username");
     };
   }, [dispatch]);
+
+  const messageEndRef = useRef(null);
+
+  useEffect(() => {
+    // Scroll to the bottom whenever messages change
+    messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
 
   const sendMessage = () => {
     if (messageInput.trim()) {
@@ -40,14 +47,18 @@ function Chat() {
         isSender: true,
         userName,
       };
-      socket.emit("send-message", message);
+      socket.emit("send-message", { message, room });
       dispatch(updated(message));
       setMessageInput("");
     }
   };
 
+  const joinRome = () => {
+    socket.emit("join-room", room);
+  };
+
   const handleDelete = (id) => {
-    socket.emit("delete-message", id);
+    socket.emit("delete-message", { id, room });
     dispatch(deleteMessage(id));
   };
 
@@ -70,27 +81,47 @@ function Chat() {
               >
                 {msg.text}
               </p>
+              <div ref={messageEndRef} />
             </div>
           ))}
         </div>
       </main>
       <footer>
         <div className="flex justify-center items-center">
-          <input
-            className="border w-3/4 p-2 rounded-lg"
-            type="text"
-            name=""
-            id=""
-            value={messageInput}
-            onChange={(e) => setMessageInput(e.target.value)}
-          />
-          <button
-            type="button"
-            className="border bg-blue-500 m-2 border-solid p-2 rounded-lg"
-            onClick={sendMessage}
-          >
-            send
-          </button>
+          <div>
+            <input
+              className="border w-3/4 p-2 rounded-lg"
+              type="text"
+              name=""
+              id=""
+              value={messageInput}
+              onChange={(e) => setMessageInput(e.target.value)}
+            />
+            <button
+              type="button"
+              className="border bg-blue-500 m-2 border-solid p-2 rounded-lg"
+              onClick={sendMessage}
+            >
+              send
+            </button>
+          </div>
+          <div>
+            <input
+              className="border w-3/4 p-2 rounded-lg"
+              type="text"
+              name=""
+              id=""
+              value={room}
+              onChange={(e) => setRoom(e.target.value)}
+            />
+            <button
+              type="button"
+              className="border bg-blue-500 m-2 border-solid p-2 rounded-lg"
+              onClick={joinRome}
+            >
+              send
+            </button>
+          </div>
         </div>
       </footer>
     </>
